1ab7e33edd3a3c77edeedc6c6063657616d95687
Prem_Kumar <krprem514@gmail.com>
2024-08-13 09:28:44 +0530

    ver

--- Variable_and_Data_Type/type_conversion.java
+++ Variable_and_Data_Type/type_conversion.java
@@ -3,6 +3,6 @@ public class type_conversion {
     public static void main(String[] args) {
         int a = 5;
         float b=a;
-        System.out.println(b+" "+a);
+        System.out.println(b+"and"+a);
     }
 }
