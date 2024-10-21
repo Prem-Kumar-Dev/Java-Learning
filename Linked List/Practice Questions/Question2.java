e15b4388367a5ebc78996ec389d86ecb0a09c294
Prem-Kumar-Dev <krprem514@gmail.com>
2024-10-22 01:45:59 +0530

    22 oct update pq

--- Linked List/Practice Questions/Question2.java
+++ Linked List/Practice Questions/Question2.java
@@ -1,25 +1,108 @@
 //delete m numbers of node after n node;
-import java.util.LinkedList;
+
+import java.util.Scanner;
 
 public class Question2 {
 
-    public void removeMfromN(){
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        Node newNode= new Node(data);
+        if (head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+        return;
 
     }
 
-    public static void main(String[] args) {
-        LinkedList<Integer> ll = new LinkedList<>();
+    public static void printLL(){
+        if (head==null){
+            System.out.println("Empty LL!");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
 
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(3);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.addLast(6);
-        ll.addLast(7);
-        ll.addLast(8);
-        System.out.println(ll);
-        Node head=ll.peekFirst();
-        System.out.println(head.next);
+
+    public void removeMfromN(int m, int n){
+        Node temp=head;
+
+
+        while(temp!=null) {
+            //treversing to n number node:
+            int count=1;
+            while (count != n && temp!=null) {
+                if(temp==null){
+                    return;
+                }
+
+                temp = temp.next;
+                count++;
+            }
+            if (temp==null){
+                return;
+            }
+            count = 0;
+            //same as removal from mid but added counter;
+            while (count != m && temp!=null) {
+                if (temp == head) {
+                    head = head.next;
+                } else{
+                    temp.next = temp.next.next;
+                }
+                count++;
+
+
+            }
+            temp=temp.next;
+        }
+        }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter value of M:");
+        int m = sc.nextInt();
+
+        System.out.println("Enter value of N:");
+        int n = sc.nextInt();
+
+        Question2 ll = new Question2();
+        ll.addFirst(10);
+        ll.addFirst(9);
+        ll.addFirst(8);
+        ll.addFirst(7);
+        ll.addFirst(6);
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        printLL();
+        ll.removeMfromN(m,n);
+        printLL();
+
+
+
+
+
     }
 }
