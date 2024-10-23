55d6aa0b329867095d6acbfbe68a4d0092fcb413
Prem-Kumar-Dev <krprem514@gmail.com>
2024-10-23 23:14:21 +0530

    .class files for backup

--- Test/test1.java
+++ Test/test1.java
@@ -1,9 +1,16 @@
 public class test1 {
+
+
     public static void main(String[] args) {
-        int a=65;
-        char b=(char)a;
-        String c=String.valueOf(b);
-        System.out.println(b);
-        System.out.println(c);
+
+        int low=1;
+        int high=999999999;
+
+        if (low%2!=0 || high%2!=0){
+            System.out.println(((high - low) / 2) + 1);
+        }else{
+            System.out.println(((high-low)/2));
+        }
     }
 }
+
